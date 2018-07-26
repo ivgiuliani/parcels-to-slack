c852a36d5bda87b28887c7d3aa5d2e99e3a6588a
Ivan Giuliani <ivan@gocardless.com>
2018-07-26 13:47:33 +0100

    Fix service account path

--- backend/index.py
+++ backend/index.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
-SERVICE_ACCOUNT_PATH = os.path.join(ROOT, "service_account.json")
+SERVICE_ACCOUNT_PATH = os.path.join(ROOT, "../../service_account.json")
 NAME_LIST_PATH = os.path.join(ROOT, "name_list.txt")
 
 
