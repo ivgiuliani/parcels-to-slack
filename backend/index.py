a8ece68c2e73cf62f451658e4798129d5d8baa49
Ivan Giuliani <ivan@gocardless.com>
2018-07-26 13:40:59 +0100

    Get the images from the internet

--- backend/index.py
+++ backend/index.py
@@ -1,15 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from NameMatcher import NameMatcher
 from ocr import get_text_from_image
 
+import os
+
+
 app = Flask(__name__)
+
+ROOT = os.path.abspath(__file__)
+SERVICE_ACCOUNT_PATH = os.path.join(ROOT, "service_account.json")
+NAME_LIST_PATH = os.path.join(ROOT, "name_list.txt")
+
 
 @app.route("/")
 def hello():
     return jsonify({"Hello": "World"})
 
+
 @app.route("/submit_image", methods=['POST'])
 def submit_image():
-    text = get_text_from_image(service_account_path, image_buff.read(), preprocess_arg)
-    nm = NameMatcher(path=names_arg)
+    text = get_text_from_image(SERVICE_ACCOUNT_PATH, request.data)
+    nm = NameMatcher(path=NAME_LIST_PATH)
     name = nm.find_name_in_blob_of_text(text)
+
+    return jsonify({
+        "name": name,
+    })
