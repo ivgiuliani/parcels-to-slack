dfeb4f3e4da77e899707cece3272a159c1c257a4
Luca Zanconato <lucazanconato@gocardless.com>
2018-07-26 12:17:00 +0100

    Added camera selector

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,11 +1,17 @@
 (function () {
     'use strict';
 
+    // globals
+	const video = document.getElementById('video');
+
     function getBase64Image(img) {
 	    // Create an empty canvas element
 	    var canvas = document.createElement('canvas');
-	    canvas.width = img.width;
-	    canvas.height = img.height;
+
+	    // TODO: get rid of the video selector
+	    canvas.width = $('#video').width();
+	    canvas.height = $('#video').height();
+	    console.log(canvas);
 
 	    // Copy the image contents to the canvas
 	    var ctx = canvas.getContext('2d');
@@ -20,26 +26,55 @@
 	    return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
 	}
 
-    // Grab elements, create settings, etc.
-	const video = document.getElementById('video');
+	navigator.mediaDevices.enumerateDevices()
+	  .then(gotDevices).then(getStream).catch(handleError);
 
-	// Get access to the camera!
-	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-	    // Not adding `{ audio: true }` since we only want video now
-	    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-	        video.srcObject = stream;
-	        video.play();
-	    });
+	videoSelect.onchange = getStream;
+
+	function gotDevices(deviceInfos) {
+	  for (let i = 0; i !== deviceInfos.length; ++i) {
+	    const deviceInfo = deviceInfos[i];
+	    const option = document.createElement('option');
+	    option.value = deviceInfo.deviceId;
+	    if (deviceInfo.kind === 'videoinput') {
+	      option.text = deviceInfo.label || 'camera ' +
+	        (videoSelect.length + 1);
+	      videoSelect.appendChild(option);
+	    } else {
+	      console.log('Found another kind of device: ', deviceInfo);
+	    }
+	  }
 	}
 
-	// Elements for taking the snapshot
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
+	function getStream() {
+	  if (window.stream) {
+	    window.stream.getTracks().forEach(function(track) {
+	      track.stop();
+	    });
+	  }
+
+	  const constraints = {
+	    video: {
+	      deviceId: {exact: videoSelect.value}
+	    }
+	  };
+
+	  navigator.mediaDevices.getUserMedia(constraints).
+	    then(gotStream).catch(handleError);
+	}
+
+	function gotStream(stream) {
+	  window.stream = stream; // make stream available to console
+	  video.srcObject = stream;
+	}
+
+	function handleError(error) {
+	  console.error('Error: ', error);
+	}
 
 	// Trigger photo take
-	document.getElementById("snap").addEventListener("click", function() {
-		context.drawImage(video, 0, 0, 320, 240);
-		$("#rawimage").text(getBase64Image(video));
+	document.getElementById('snap').addEventListener('click', function() {
+		console.log(getBase64Image(video));
 		// video.pause();
 	});
 })();
