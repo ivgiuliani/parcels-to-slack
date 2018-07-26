42cc8a5e25d20a0771c97b6b5a55a21cc277ff71
dario <dario@gocardless.com>
2018-07-26 13:20:36 +0100

    new ocr

---
+++ backend/NameMatcher.py
@@ -0,0 +1,31 @@
+class NameMatcher:
+
+    def __init__(self, path="name_list.txt"):
+        self.__path = path
+        self.refresh()
+
+    def refresh(self):
+        with open(self.__path) as f:
+            self.__name_list = f.read().lower().splitlines()
+
+    def print(self):
+        for index, name in enumerate(self.__name_list):
+            print("{} - {}".format(index, name))
+
+    def find_name_in_blob_of_text(self, blob_of_text):
+
+        tokens = set(" ".join([" ".join(line.split()) for line in blob_of_text.lower().splitlines() if line]).split(" "))
+
+        name_found = None
+        for name in self.__name_list:
+            name_set = set(name.split())
+            if tokens & name_set:
+                return name
+
+        if name_found is None:
+            raise Exception("No name found!")
+
+
+if __name__ == "__main__":
+    nm = NameMatcher()
+    nm.print()
