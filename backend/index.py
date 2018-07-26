897523b7ad04547d2e487dd7875d5887e08c7ec3
dario <dario@gocardless.com>
2018-07-26 13:24:26 +0100

    submit image endpoint v0

--- backend/index.py
+++ backend/index.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify
+from NameMatcher import NameMatcher
+from ocr import get_text_from_image
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return jsonify({"Hello": "World"})
+
+@app.route("/submit_image", methods=['POST'])
+def submit_image():
+    text = get_text_from_image(service_account_path, image_buff.read(), preprocess_arg)
+    nm = NameMatcher(path=names_arg)
+    name = nm.find_name_in_blob_of_text(text)
